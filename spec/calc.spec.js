3383ac2e2c94eec79fd6d0ac91fc41aeb0efdc45
kgauzampe <kgauza.mpe@gmail.com>
2019-11-19 15:18:14 +0200

    renamed

--- spec/calc.spec.js
+++ spec/calc.spec.js
@@ -15,8 +15,8 @@ describe("Check if string is null", function () {
 
     })
     it("check if it separates using a coma", function () {
-        expect(calculate.Add("1,7")).toEqual(8);
-
+        expect(calculate.Add("1,7")).toEqual(8); 
+ 
     })
     it("check if it separates using other special characters", function () {
         expect(calculate.Add("1)(*&^%),7")).toEqual(8);
